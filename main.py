44b2e1ff1bfa4b99a21399a82eacfac6e438dbd7
Desiree Mendes <desireefrost@live.ca>
2022-02-26 00:33:21 -0500

    Create turtles, create race random distance

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
-# import turtle as t
-# import random
-
-
-# jace = t.Turtle()
-# t.colormode(255)
-
-# def random_colour():
-#   r = random.randint(0, 255)
-#   g = random.randint(0, 255)
-#   b = random.randint(0, 255)
-#   colour = (r, g, b)
-#   return colour
-
-# directions = [0, 90, 100, 270]
-
-# jace.forward(30)
-# jace.setheading(random.choice(directions))
-# print(jace)
-# jace.shape("turtle")
-# jace.pensize(15)
-# jace.speed("fastest")
-
-# for _ in range(200):
-#   jace.color(random_colour())
-#   jace.forward(30)
-#   jace.setheading(random.choice(directions))
-
 from turtle import Turtle, Screen
+import random
 
-
-
-jace = Turtle()
+is_race_on = False
 screen = Screen()
-# image = "race.png"
-# screen.addshape(image)
+screen.bgcolor("white")
+screen.bgpic('sky.gif')
 screen.setup(width=500, height=400)
-jace.screen.bgcolor("white")
-jace.screen.bgpic('sky.gif')
-user_bet = screen.textinput(title="Make your bets", prompt="Which turtle will win the race? Enter colour: ")
-print(user_bet)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
+#Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_forward():
-  jace.forward(10)
+if user_bet:
+    is_race_on = True
 
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
 screen.exitonclick()
