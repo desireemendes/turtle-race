068db47e153f1780df5e83c6ae1b84670ae21678
Desiree Mendes <desireefrost@live.ca>
2022-02-25 22:37:10 -0500

    add speed, size, random colours

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from turtle import *
+import turtle as t
+import random
 
-jace = Turtle()
-des = Turtle()
-jabs = Turtle()
 
+jace = t.Turtle()
+t.colormode(255)
+
+def random_colour():
+  r = random.randint(0, 255)
+  g = random.randint(0, 255)
+  b = random.randint(0, 255)
+  random_colour = (r, g, b)
+  return random_colour
+
+directions = [0, 90, 100, 270]
+
+# jace.forward(30)
+# jace.setheading(random.choice(directions))
 print(jace)
 jace.shape("turtle")
-jace.color("coral")
-des.shape("turtle")
-des.color("blue")
-jabs.shape("turtle")
-jabs.color("yellow")
-# jace.forward(100)
+jace.pensize(15)
+jace.speed("fastest")
 
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-for _ in range(4):
-  jace.forward(100)
-  jace.left(90)
-  des.backward(100)
-  des.right(90)
+for _ in range(200):
+  jace.color(random_colour())
+  jace.forward(30)
+  jace.setheading(random.choice(directions))
